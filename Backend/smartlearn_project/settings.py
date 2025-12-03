b52775a914faa577b9cd6990b92a0afc8ebb1a09
sameer-ahmed123 <sameerkhanjan123@gmail.com>
2025-12-04 01:50:06 +0500

    added mysql_db connection

--- Backend/smartlearn_project/settings.py
+++ Backend/smartlearn_project/settings.py
@@ -92,8 +92,11 @@ WSGI_APPLICATION = 'smartlearn_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get("DB_NAME"),  
+        'USER': os.environ.get("DB_USER"),  
+        'PASSWORD': os.environ.get("DB_PASS"), 
+        'PORT': os.environ.get("DB_PORT"),
     }
 }
 
