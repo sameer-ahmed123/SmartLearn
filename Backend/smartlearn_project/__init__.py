f55bd0e7358173e5a1bcf65a9682ac4298bbbd2e
sameer-ahmed123 <sameerkhanjan123@gmail.com>
2026-02-21 21:43:41 +0500

    NA

--- Backend/smartlearn_project/__init__.py
+++ Backend/smartlearn_project/__init__.py
@@ -1,4 +1,9 @@
+import pymysql
+
 # Loads Celery app when our Django Project starts 
 from .celery import app as celery_app
+
+pymysql.version_info = (2, 2, 7, "final", 0)
+pymysql.install_as_MySQLdb()
 
 __all__ = ('celery_app',)
