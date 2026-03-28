a98afce78213fab7f60e458d86eafecf0c5f5068
sameer-ahmed123 <sameerkhanjan123@gmail.com>
2026-03-28 21:24:48 +0500

    Temporarily moved to neon db

--- Backend/smartlearn_project/settings.py
+++ Backend/smartlearn_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os 
 from pathlib import Path
+from urllib.parse import parse_qsl, urlparse
 import cloudinary # type:ignore
 import cloudinary.uploader # type:ignore
 import cloudinary.api # type:ignore
@@ -115,21 +116,38 @@ WSGI_APPLICATION = 'smartlearn_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+########### moved to neon Db --temporary
+tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': tmpPostgres.path.replace('/', ''),
+        'USER': tmpPostgres.username,
+        'PASSWORD': tmpPostgres.password,
+        'HOST': tmpPostgres.hostname,
+        'PORT': 5432,
+        'OPTIONS': dict(parse_qsl(tmpPostgres.query)),
+    }
+}
+############
+
+
 
 ######
 ##    CURRENTLY DO NOT USE THIS SETUP  
 ##    REASON ==> CRASHES THE CELERY WORKER -DOES NOT ALLOW CELERY WORKER TO CONNECT WITH DATABASE 
 ######
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get("DB_NAME"),  
-        'USER': os.environ.get("DB_USER"),  
-        'PASSWORD': os.environ.get("DB_PASS"), 
-        'PORT':os.environ.get("DB_PORT"),
-        'HOST':os.environ.get("DB_HOST"),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': os.environ.get("DB_NAME"),  
+#         'USER': os.environ.get("DB_USER"),  
+#         'PASSWORD': os.environ.get("DB_PASS"), 
+#         'PORT':os.environ.get("DB_PORT"),
+#         'HOST':os.environ.get("DB_HOST"),
+#     }
+# }
 
 
 # ######
