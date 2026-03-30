e0b2d02155f8407c1bb662b61044c9f320924384
sameer-ahmed123 <sameerkhanjan123@gmail.com>
2026-03-30 13:46:14 +0500

    added formated time to notification content

--- Backend/notifications/tasks.py
+++ Backend/notifications/tasks.py
@@ -1,14 +1,20 @@
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
-
+from django.utils import timezone
 from smartlearn_project import celery_app
 from django.contrib.auth import get_user_model
 from notifications.models import Notification
 from lectures.models import Enrollment, Lecture
 from django.contrib.contenttypes.models import ContentType
 
+channel_layer = get_channel_layer()
+
+
 @celery_app.task
 def create_bulk_notifications(course_id,verb,target_ct_id,target_id):
+    now = timezone.now()
+    # Format: "Mar 30, 08:52 AM"
+    formated_time = now.strftime("%b %d, %I:%M %p")
     User = get_user_model()
     target_ct = ContentType.objects.get_for_id(target_ct_id)
     
@@ -27,7 +33,6 @@ def create_bulk_notifications(course_id,verb,target_ct_id,target_id):
     
     Notification.objects.bulk_create(notifications)
     
-    channel_layer = get_channel_layer()
     
     for s_id in student_ids:
         group_name = f"user_notifications_{s_id}"
@@ -38,7 +43,7 @@ def create_bulk_notifications(course_id,verb,target_ct_id,target_id):
                 "content": {
                     "verb": verb,
                     "target_id": target_id,
-                    "created_at": "Just now" # You can format the real date here
+                    "created_at": formated_time
                 }
             }
         )
